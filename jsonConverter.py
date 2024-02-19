dce0448654070ac714ee628cf61c74893b9b5f94
melon <melonpan08@gmail.com>
2024-02-19 13:27:22 +0000

    [add] SPMODEを追加
    特別なときに表示を切り替えられるSPモードを追加

--- jsonConverter.py
+++ jsonConverter.py
@@ -28,12 +28,11 @@ for line in lines:
 photodict = {}
 
 for list in photolist[1:(1 + len(photolist))]:
-	if len(list) < 2:
+	if len(list) < 3:
 			continue
-	tmp = {list[0]:{photolist[0][1]:int(list[1]), photolist[0][2]:list[2]}}
+	tmp = {list[0]:{photolist[0][1]:int(list[1]), photolist[0][2]:list[2], photolist[0][3]:list[3]}}
 	photodict.update(tmp)
 
 with open(jsonfile, mode="w", encoding="utf-8") as f:
 	json.dump(photodict, f, ensure_ascii=False, indent="\t")
-
 

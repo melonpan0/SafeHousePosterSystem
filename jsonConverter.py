3819c801b9875d21f0c6dc75cb75af9b03d0e752
melon <melonpan08@gmail.com>
2024-02-19 13:42:34 +0000

    [fix] bool型がstr型として表示される不具合を修正

--- jsonConverter.py
+++ jsonConverter.py
@@ -30,6 +30,8 @@ photodict = {}
 for list in photolist[1:(1 + len(photolist))]:
 	if len(list) < 3:
 			continue
+	# True Falseを正しく変換
+	list = [True if column == "true" else False if column == "false" for column in list]
 	tmp = {list[0]:{photolist[0][1]:int(list[1]), photolist[0][2]:list[2], photolist[0][3]:list[3]}}
 	photodict.update(tmp)
 
